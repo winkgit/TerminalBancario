9286c1bbd43e846fe361df24ee4ea33b031a0ccf
winkgit <winkgnr@proton.me>
2026-05-23 16:10:39 +0000

    Implement the necessary logic on input1 according to recent commits (a3c0f76)

--- TerminalBancario/main.lua
+++ TerminalBancario/main.lua
@@ -18,20 +18,39 @@ local function mainmenu()
         print("Crie um nome de usuário para esta conta:")
         local name = io.read()
 
+        while name == "" or name == "nil" or #name < 3 do
+
+            print("Nome inválido! Tente novamente:")
+            name = io.read()
+
+        end
+
+    do
+
+        -- esse aqui demorou um tempinho até eu conseguir achar uma solução boa.
+        -- só fui pensar nessa solução abaixo quando estava tentando encaixar um ternário na lógica.
+        local namecheck = mBank.criarConta(name) or name
+        name = namecheck
+
+    end
+
         print("Crie uma senha para esta conta:")
         local password = io.read()
 
+        while password == "" or #password < 4 do
+            
+            print("Senha muito fraca! Tente novamente:")
+            password = io.read()
+
+        end
+
         print("Saldo inicial:")
-        local money = tonumber(io.read())
+        local money = io.read()
 
-        if money <= 0 then
+        while money == "" or not tonumber(money) or tonumber(money) < 0 do
 
-            repeat
-
-                print("Valores negativos são inválidos. Tente novamente:")
-                money = tonumber(io.read())
-
-            until money >= 0
+            print("Valor inválido! Tente novamente:")
+            money = io.read()
 
         end
 
@@ -39,7 +58,7 @@ local function mainmenu()
 
     end
 
-    if input == 2 then -- INCOMPLETO / MAL IMPLEMENTADO (?)
+    if input == 2 then --@INFO: MAL IMPLEMENTADO (?)
 
         print("Insira o nome de usuário da conta:")
         local name = io.read()
@@ -47,7 +66,7 @@ local function mainmenu()
         print("Insira a senha da conta:")
         local password = io.read()
 
-        local res1, msg = mBank.acessarConta(name, password)
+        mBank.acessarConta(name, password)
 
     end
 
@@ -57,7 +76,7 @@ local function mainmenu()
         local name = io.read()
 
         print("WARNING: Do you really want to delete this account? Account deletion is irreversible! (y/n)")
-        if tostring(io.read()) == "y" then local res1, msg = mBank:deletarConta(name); print(msg)
+        if tostring(io.read()) == "y" then local res1, msg = mBank.deletarConta(name); print(msg)
         else os.execute("clear"); mainmenu() end
 
     end
