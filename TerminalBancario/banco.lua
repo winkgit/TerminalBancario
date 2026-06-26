17b653cadb8a23b870a108053b664cd223a5ebb0
winkgit <winkgnr@proton.me>
2026-06-26 17:03:41 -0300

    Implement salvarContas() and carregarOuCriar() Prioritize memory-first implementations over direct I/O Remove predefined paths, use a relative path implementation Use PRESAVE as a stager for all writes, only salvarContas() should write to disk Make criarConta, acessarConta and deletarConta memory-first carregarOuCriar(): Load entire contas.txt into CONTASTXT on startup Minor optimizations

--- TerminalBancario/banco.lua
+++ TerminalBancario/banco.lua
@@ -5,21 +5,27 @@
 -- IMPORTANTE: O MÉTODO ATUAL USADO PARA SALVAR AS CONTAS NÃO É IDEAL: CADA CONTA POSSUÍ SEU BLOCO COM SUAS PROPRIEDADES. BLOCOS SÃO SEPARADOS POR LINHAS VAZIAS.
 -- Mesmo assim, vou usar esse método ruim para motivos de APRENDIZADO! Veja o fim desse código, por conta disso que a implementação se torna ineficiente.
 
-package.path = package.path .. ";/home/poweruser/lua/TerminalBancario/?.lua"
+package.path = package.path .. ";;" .. debug.getinfo(1, "S").source:sub(2):match("(.*/)") .. "?.lua"
 local mConta = require("conta")
 
 local banco = {}
-local path = "/home/poweruser/lua/contas.txt"
+
+local CONTASTXT = {}
+local PRESAVE = {}
+
+-- derive contas.txt path relative to this file
+local this_dir = debug.getinfo(1, "S").source:sub(2):match("(.*/)") or "./"
+local path = this_dir .. "contas.txt"
 
 function banco.criarConta(acc, paw, mon) -- conta, senha, money
 
     local newname
 
-    -- built-in duplicated account names checker. might be better to create a closure for this?
+    -- Built-in duplicated account name checker. might be better to create a closure for this?
     --@INFO: THIS IS COMPACT AS FUCK.
-    for line in io.lines(path) do
+    for i, v in ipairs(CONTASTXT) do
 
-        if line:sub(1, #("name_".. acc)) == ("name_".. acc) then
+        if v:sub(1, #("name_".. acc)) == ("name_".. acc) then
 
             repeat
 
@@ -34,11 +40,10 @@ function banco.criarConta(acc, paw, mon) -- conta, senha, money
 
     if not paw then return end
 
-    local path = assert(io.open(path, "a+")); io.output(path)
+    acc, paw, mon = ("name_".. acc), ("\npassword_".. acc.. ": ".. paw), ("\nbank_".. acc.. ": ".. mon.. "\n\n")
+    PRESAVE[#PRESAVE+1] = acc; PRESAVE[#PRESAVE+1] = paw; PRESAVE[#PRESAVE+1] = mon
 
-    local userinfo = ("name_".. acc.. "\npassword_".. acc.. ": ".. paw.. "\nbank_".. acc.. ": ".. mon.. "\n\n")
-
-    io.write(userinfo):close()
+    banco.salvarContas()
 
 end
 
@@ -47,11 +52,11 @@ function banco.acessarConta(acc, paw) -- LOADS ACCOUNT'S CREDENTIALS WITH CONTA:
     local propmatch
     local foundName, foundPassowrd, money = false, false, nil
 
-    for line in io.lines(path) do
+    for i, v in ipairs(CONTASTXT) do
 
         propmatch = ("name_".. acc)
 
-        if line:sub(1, #propmatch) == propmatch then -- NAME FIRST
+        if v:sub(1, #propmatch) == propmatch then -- NAME FIRST
             
             foundName = true
 
@@ -59,15 +64,15 @@ function banco.acessarConta(acc, paw) -- LOADS ACCOUNT'S CREDENTIALS WITH CONTA:
 
         propmatch = ("password_".. acc.. ": ".. paw)
 
-        if line:sub(1, #propmatch) == propmatch then -- PASSWORD SECOND
+        if v:sub(1, #propmatch) == propmatch then -- PASSWORD SECOND
 
             foundPassowrd = true
 
         end
 
-        if line:sub(1, #("bank_".. acc)) == ("bank_".. acc) then -- MONEY LAST
+        if v:sub(1, #("bank_".. acc)) == ("bank_".. acc) then -- MONEY LAST
 
-            money = tonumber(line:match("(%d+)")) --@FIXME: MUST HANDLE NUMBERS IN ACCOUNT'S NAME
+            money = tonumber(v:match("(%d+)")) --@FIXME: MUST HANDLE NUMBERS IN ACCOUNT'S NAME
 
         end
 
@@ -89,28 +94,28 @@ end
 
 function banco.deletarConta(acc)
 
-    local file = {}
     local skip = false
     local found = false
+    local propmatch = ("name_".. acc)
 
-    for line in io.lines(path) do
+    for i, v in ipairs(CONTASTXT) do
 
         if skip then
 
-            if line == "" then
+            if v == "" then
 
                 skip = false
 
             end
 
-        elseif line == "name_".. acc then
+        elseif v:sub(1, #propmatch) == propmatch then
 
             found = true
             skip = true
 
         else
 
-            table.insert(file, line)
+            table.insert(PRESAVE, v)
 
         end
 
@@ -118,30 +123,53 @@ function banco.deletarConta(acc)
 
     if not found then return false, "Account does not exist!" end
 
-    local path = assert(io.open(path, "w"))
-    path:write(table.concat(file, "\n"))
-
-    if #file > 0 then path:write("\n"):close(); return true, "Account deleted successfully!" end
-
-    path:close()
+    if #PRESAVE > 0 then table.insert(PRESAVE, "\n") return true, "Account deleted successfully!" end -- IT IS SET FOR DELETION, NOT DELETED IMMEDIATELY
 
 end
 
 -- Objetivo: Persistir (salvar) todas as contas existentes no arquivo contas.txt.
 function banco.salvarContas()
 
+    path = assert(io.open(path, "a+"))
 
+    for i, v in ipairs(PRESAVE) do
+       
+        path:write(v)
 
+    end
+
+    path:close()
+    PRESAVE = {}
+    CONTASTXT = {}
+
+    print("SALVARCONTAS RODOU")
 
 end
 
--- Objetivo: Carregar as contas do arquivo contas.txt quando o programa inicia. Se o arquivo não existir, cria um novo (vazio).
+-- Carrega as contas do arquivo contas.txt quando o programa inicia. Se o arquivo não existir, cria um novo (vazio).
 function banco.carregarOuCriar()
 
+    if not io.open(path, "r") then -- cria se nao houver
 
+        io.open(path, "w"):close()
 
+        return true
 
+    else
+
+        for line in io.lines(path) do  -- carrega a tabela na memoria
+
+            table.insert(CONTASTXT, line)
+
+        end
+
+        return CONTASTXT
+
+    end
 
 end
 
 return banco
+
+-- CONVERTER IMPLEMENTAÇÕES DE I/O (DISCO) PARA INTERAÇÕES DE MEMÓRIA
+-- FAZER CHECAGEM DE ATUALIZAÇÃO PARA VER SE TUDO ESTÁ FEITO E PRONTO PARA PUSHAR
